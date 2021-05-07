f4906b65553363a855321c85eb4815ea987a4cc0
Gilberto Kreisler <gkreisler5@hotmail.com>
2021-05-06 23:13:37 -0300

    feat: mark circle filled

--- libs/helpers/helper.cpp
+++ libs/helpers/helper.cpp
@@ -11,7 +11,7 @@ void helpers::createImageFile(cv::Mat &image, double time, std::string fileName)
 
 void helpers::drawObject(int x, int y, cv::Scalar color, cv::Mat &dst)
 {
-    cv::circle(dst, cv::Point(x, y), 5, color, 2);
+    cv::circle(dst, cv::Point(x, y), 5, color, cv::FILLED);
 
     cv::putText(dst, std::to_string(x) + "," + std::to_string(y), cv::Point(x,y), 1, 1, color, 2);
 }
