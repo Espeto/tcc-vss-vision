f1b181874f5d1fffb0d1b0b6b1f72bfb7e83b266
Gilberto Kreisler <gkreisler5@hotmail.com>
2021-05-10 13:48:33 -0300

    feat: added other robots to track

--- main.cpp
+++ main.cpp
@@ -54,41 +54,34 @@ struct CallbackData
     Mat *outDir;
 };
 
-int H1DefMin = 0, H1DefMax = 179;
-int S1DefMin = 0, S1DefMax = 255;
-int V1DefMin = 0, V1DefMax = 255;
+// Team
+int HDefMin = 0, HDefMax = 179;
+int SDefMin = 0, SDefMax = 255;
+
+// Players
 int H1DirMin = 0, H1DirMax = 179;
 int S1DirMin = 0, S1DirMax = 255;
-int V1DirMin = 0, V1DirMax = 255;
 
-int H2DefMin = 0, H2DefMax = 179;
-int S2DefMin = 0, S2DefMax = 255;
-int V2DefMin = 0, V2DefMax = 255;
 int H2DirMin = 0, H2DirMax = 179;
 int S2DirMin = 0, S2DirMax = 255;
-int V2DirMin = 0, V2DirMax = 255;
 
-int H3DefMin = 0, H3DefMax = 179;
-int S3DefMin = 0, S3DefMax = 255;
-int V3DefMin = 0, V3DefMax = 255;
 int H3DirMin = 0, H3DirMax = 179;
 int S3DirMin = 0, S3DirMax = 255;
-int V3DirMin = 0, V3DirMax = 255;
 
 const std::string original_window = "Original";
 
 const std::string window_ally_1 = "Ally 1";
 const std::string out_dir_1 = "Ally Dir 1";
 
-/* const std::string window_ally_2 = "Ally 2";
 const std::string out_dir_2 = "Ally Dir 2";
 
-const std::string window_ally_3 = "Ally 3";
 const std::string out_dir_3 = "Ally Dir 3";
 
+/*
 const std::string window_enemy_1 = "Enemy 1";
 const std::string window_enemy_2 = "Enemy 2";
-const std::string window_enemy_3 = "Enemy 3"; */
+const std::string window_enemy_3 = "Enemy 3"; 
+*/
 
 Colors *colors;
 
@@ -102,12 +95,14 @@ int main()
 
     Mat allyDefOut1, allyDirOut1;
 
-    std::vector<cv::Scalar> draw_colors = {cv::Scalar(233,66,245), cv::Scalar(40,115,72), cv::Scalar(118,145,48)};
+    std::vector<cv::Scalar> draw_colors = {cv::Scalar(233, 66, 245), cv::Scalar(40, 115, 72), cv::Scalar(118, 145, 48)};
 
-    /* 
+    int phc = 1;
+
     Mat allyDefOut2, allyDirOut2;
     Mat allyDefOut3, allyDirOut3;
 
+    /*
     Mat enemyOut1, enemyOut2, enemyOut3 
     */
 
@@ -120,12 +115,12 @@ int main()
         std::cout << "Erro ao abrir vídeo" << std::endl;
         exit(-1);
     }
-    
+
     cap >> original;
 
     /* INICIALIZAÇÕES */
 
-    std::vector<CallbackData *> ally_data = {new CallbackData}; //, new CallbackData, new CallbackData};
+    std::vector<CallbackData *> ally_data = {new CallbackData, new CallbackData, new CallbackData};
     //std::vector<CallbackData *> enemy_data = {new CallbackData, new CallbackData, new CallbackData};
 
     colors = Global::getColors();
@@ -135,20 +130,18 @@ int main()
 
     allyDefOut1 = Mat::zeros(original.size(), CV_8UC1);
     allyDirOut1 = Mat::zeros(original.size(), CV_8UC1);
-
-    /*
-    allyDefOut2 = Mat::zeros(original.size(), CV_8UC1);
+    
     allyDirOut2 = Mat::zeros(original.size(), CV_8UC1);
 
-    allyDefOut3 = Mat::zeros(original.size(), CV_8UC1);
     allyDirOut3 = Mat::zeros(original.size(), CV_8UC1);
 
+    /*
     enemyOut1 = Mat::zeros(original.size(), CV_8UC1);
     enemyOut2 = Mat::zeros(original.size(), CV_8UC1);
     enemyOut3 = Mat::zeros(original.size(), CV_8UC1);
     */
 
-    for (int i = 0; i < 1; i++)
+    for (int i = 0; i < 3; i++)
     {
         ally_data[i]->original = &hsvImage;
         ally_data[i]->id = i;
@@ -157,11 +150,11 @@ int main()
     ally_data[0]->outDef = &allyDefOut1;
     ally_data[0]->outDir = &allyDirOut1;
 
-    //ally_data[1]->outDef = &allyDefOut2;
-    //ally_data[1]->outDir = &allyDirOut2;
+    ally_data[1]->outDef = &allyDefOut2;
+    ally_data[1]->outDir = &allyDirOut2;
 
-    //ally_data[2]->outDef = &allyDefOut3;
-    //ally_data[2]->outDir = &allyDirOut3;
+    ally_data[2]->outDef = &allyDefOut3;
+    ally_data[2]->outDir = &allyDirOut3;
 
     /* FIM INICIALIZAÇÕES */
 
@@ -169,19 +162,16 @@ int main()
     namedWindow(original_window, WINDOW_NORMAL);
 
     namedWindow(window_ally_1, WINDOW_NORMAL);
-    //namedWindow(window_ally_2);
-    //namedWindow(window_ally_3);
 
     namedWindow(out_dir_1, WINDOW_NORMAL);
-    //namedWindow(out_dir_2);
-    //namedWindow(out_dir_3);
-
-    namedWindow("Segex");
+    namedWindow(out_dir_2, WINDOW_NORMAL);
+    namedWindow(out_dir_3, WINDOW_NORMAL);
 
     namedWindow("Run Track");
 
-    namedWindow("ThresholdSegTeam");
-    namedWindow("ThresholdSegPlayer");
+    // namedWindow("ThresholdSegTeam");
+    // namedWindow("ThresholdSegPlayer");
+
     /*
     namedWindow(window_enemy_1);
     namedWindow(window_enemy_2);
@@ -191,33 +181,27 @@ int main()
 
     // Chamando as funções
     robot1TrackbarsCreator(window_ally_1, (void *)ally_data[0]);
-    /* robot2TrackbarsCreator(window_ally_2, (void*) ally_data[1]);
-    robot3TrackbarsCreator(window_ally_3, (void*) ally_data[2]); */
+    robot2TrackbarsCreator(out_dir_2, (void*) ally_data[1]);
+    robot3TrackbarsCreator(out_dir_3, (void*) ally_data[2]);
 
     trackObjs();
+
+    PreProcessPipe1 preprocess1 = PreProcessPipe1();
+    auto segmentation1 = std::make_unique<SegmentationPipe1>();
+    auto extraction1 = std::make_unique<ExtractionPipe1>();
+    SegexPipe1 segex1 = SegexPipe1(segmentation1.get(), extraction1.get());
 
     while (1)
     {
 
         cap >> original;
 
-        if(original.empty()) break;
+        if (original.empty())
+            break;
 
-        imshow(window_ally_1, allyDefOut1);
-        imshow(out_dir_1, allyDirOut1);
-
-        //imshow(window_ally_2, allyDefOut2);
-        //imshow(out_dir_2, allyDirOut2);
-
-        //imshow(window_ally_3, allyDefOut3);
-        //imshow(out_dir_3, allyDirOut3);
 
         if (val)
         {
-            PreProcessPipe1 preprocess1 = PreProcessPipe1();
-            auto segmentation1 = std::make_unique<SegmentationPipe1>();
-            auto extraction1   = std::make_unique<ExtractionPipe1>();
-            SegexPipe1 segex1 = SegexPipe1(segmentation1.get(), extraction1.get());
 
             preprocess1.execute(original, filteredImg);
             segex1.execute(filteredImg);
@@ -225,7 +209,8 @@ int main()
         else
         {
             //calibragem
-            while(!val) {
+            while (!val)
+            {
 
                 PreProcess::applyBlur(original, original, 3, PreProcess::smoothType::MEDIAN);
 
@@ -234,20 +219,42 @@ int main()
                 imshow(window_ally_1, allyDefOut1);
                 imshow(out_dir_1, allyDirOut1);
 
+                imshow(out_dir_2, allyDirOut2);
+
+                imshow(out_dir_3, allyDirOut3);
+
                 imshow(original_window, original);
 
                 waitKey(10);
             }
         }
 
-        for (int i = 0; i < 1; i++)
-            {
-                Robo *r = Global::getAlliedRobots()[i];
+        for (int i = 0; i < 3; i++)
+        {
+            Robo *r = Global::getAlliedRobots()[i];
+            cv::Scalar point_color;
 
-                helpers::drawObject(r->getPosX(), r->getPosY(), cv::Scalar(100,0,255), original);
+            switch (i)
+            {
+            case 0:
+                point_color = cv::Scalar(0, 0, 255);
+                break;
+            
+            case 1:
+                point_color = cv::Scalar(0, 255, 0);
+                break;
+
+            case 2:
+                point_color = cv::Scalar(255,0,0);
+                break;
             }
 
+            helpers::drawObject(r->getPosX(), r->getPosY(), point_color, original);
+        }
+
         imshow(original_window, original);
+
+        helpers::createImageFile(original, phc++, "frames/frame");
 
         waitKey(10);
     }
@@ -278,10 +285,10 @@ void enemyTrackbarsCreator(std::string window_name, void *data)
 void robot1TrackbarsCreator(std::string window_name, void *data)
 {
 
-    createTrackbar("H Def Min", window_name, &H1DefMin, 179, setHDefMin, data);
-    createTrackbar("H Def Max", window_name, &H1DefMax, 179, setHDefMax, data);
-    createTrackbar("S Def Min", window_name, &S1DefMin, 255, setSDefMin, data);
-    createTrackbar("S Def Max", window_name, &S1DefMax, 255, setSDefMax, data);
+    createTrackbar("H Def Min", window_name, &HDefMin, 179, setHDefMin, data);
+    createTrackbar("H Def Max", window_name, &HDefMax, 179, setHDefMax, data);
+    createTrackbar("S Def Min", window_name, &SDefMin, 255, setSDefMin, data);
+    createTrackbar("S Def Max", window_name, &SDefMax, 255, setSDefMax, data);
 
     createTrackbar("H Dir Min", window_name, &H1DirMin, 179, setHDirectMin, data);
     createTrackbar("H Dir Max", window_name, &H1DirMax, 179, setHDirectMax, data);
@@ -292,11 +299,6 @@ void robot1TrackbarsCreator(std::string window_name, void *data)
 void robot2TrackbarsCreator(std::string window_name, void *data)
 {
 
-    createTrackbar("H Def Min", window_name, &H2DefMin, 179, setHDefMin, data);
-    createTrackbar("H Def Max", window_name, &H2DefMax, 179, setHDefMax, data);
-    createTrackbar("S Def Min", window_name, &S2DefMin, 255, setSDefMin, data);
-    createTrackbar("S Def Max", window_name, &S2DefMax, 255, setSDefMax, data);
-
     createTrackbar("H Dir Min", window_name, &H2DirMin, 179, setHDirectMin, data);
     createTrackbar("H Dir Max", window_name, &H2DirMax, 179, setHDirectMax, data);
     createTrackbar("S Dir Min", window_name, &S2DirMin, 255, setSDirectMin, data);
@@ -305,11 +307,6 @@ void robot2TrackbarsCreator(std::string window_name, void *data)
 
 void robot3TrackbarsCreator(std::string window_name, void *data)
 {
-
-    createTrackbar("H Def Min", window_name, &H3DefMin, 179, setHDefMin, data);
-    createTrackbar("H Def Max", window_name, &H3DefMax, 179, setHDefMax, data);
-    createTrackbar("S Def Min", window_name, &S3DefMin, 255, setSDefMin, data);
-    createTrackbar("S Def Max", window_name, &S3DefMax, 255, setSDefMax, data);
 
     createTrackbar("H Dir Min", window_name, &H3DirMin, 179, setHDirectMin, data);
     createTrackbar("H Dir Max", window_name, &H3DirMax, 179, setHDirectMax, data);
