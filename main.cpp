d16c937e0b0e302f869a2b6a0ee1b00d49fa6120
Gilberto Kreisler <gkreisler5@hotmail.com>
2021-05-21 04:39:48 -0300

    fix: dynamic range H channel

--- main.cpp
+++ main.cpp
@@ -78,24 +78,24 @@ struct CallbackData
 };
 
 // Team
-int HDefMin = 0, HDefMax = 179;
+int HDefMin = 0, HDefMax = 180;
 int SDefMin = 0, SDefMax = 255;
 int VDefMin = 0, VDefMax = 255;
 
 // Players
-int H1DirMin = 0, H1DirMax = 179;
+int H1DirMin = 0, H1DirMax = 180;
 int S1DirMin = 0, S1DirMax = 255;
 int V1DirMin = 0, V1DirMax = 255;
 
-int H2DirMin = 0, H2DirMax = 179;
+int H2DirMin = 0, H2DirMax = 180;
 int S2DirMin = 0, S2DirMax = 255;
 int V2DirMin = 0, V2DirMax = 255;
 
-int H3DirMin = 0, H3DirMax = 179;
+int H3DirMin = 0, H3DirMax = 180;
 int S3DirMin = 0, S3DirMax = 255;
 int V3DirMin = 0, V3DirMax = 255;
 
-int ballHMin = 0, ballHMax = 179;
+int ballHMin = 0, ballHMax = 180;
 int ballSMin = 0, ballSMax = 255;
 int ballVMin = 0, ballVMax = 255;
 
@@ -122,18 +122,15 @@ int main()
     VideoCapture cap;
 
     Mat original, hsvImage, filteredImg;
-
     Mat allyDefOut1, allyDirOut1;
-
-    std::vector<cv::Scalar> draw_colors = {cv::Scalar(233, 66, 245), cv::Scalar(40, 115, 72), cv::Scalar(118, 145, 48)};
-
-    int phc = 1;
-
     Mat allyDefOut2, allyDirOut2;
     Mat allyDefOut3, allyDirOut3;
     Mat ballOut;
     Mat enemyOut; 
+
+    CallbackData ballCallback, enemy_data;
    
+   int phc = 1;
 
     std::string videoPath = "../videos/teste_final.webm";
 
@@ -156,13 +153,8 @@ int main()
     SegexPipe1 segex1 = SegexPipe1(segmentation1.get(), extraction1.get());
 
     std::vector<CallbackData *> ally_data = {new CallbackData, new CallbackData, new CallbackData};
-    
-    CallbackData ballCallback, enemy_data;
 
     colors = Global::getColors();
-
-    //auto enemies = Global::getEnemyRobots();
-    //auto ball = Global::getBall();
 
     allyDefOut1 = Mat::zeros(original.size(), CV_8UC1);
     allyDirOut1 = Mat::zeros(original.size(), CV_8UC1);
@@ -331,12 +323,12 @@ void trackObjs()
 
 void enemyTrackbarsCreator(std::string window_name, void *data)
 {
-    static int HEnemyDefMin = 0, HEnemyDefMax = 179;
+    static int HEnemyDefMin = 0, HEnemyDefMax = 180;
     static int SEnemyDefMin = 0, SEnemyDefMax = 255;
     static int VEnemyDefMin = 0, VEnemyDefMax = 255;
 
-    createTrackbar("H Def Min", window_name, &HEnemyDefMin, 255, setHEnemyMin, data);
-    createTrackbar("H Def Max", window_name, &HEnemyDefMax, 179, setHEnemyMax, data);
+    createTrackbar("H Def Min", window_name, &HEnemyDefMin, 180, setHEnemyMin, data);
+    createTrackbar("H Def Max", window_name, &HEnemyDefMax, 180, setHEnemyMax, data);
     createTrackbar("S Def Min", window_name, &SEnemyDefMin, 255, setSEnemyMin, data);
     createTrackbar("S Def Max", window_name, &SEnemyDefMax, 255, setSEnemyMax, data);
     createTrackbar("V Def Min", window_name, &VEnemyDefMin, 255, setVEnemyMin, data);
@@ -346,15 +338,15 @@ void enemyTrackbarsCreator(std::string window_name, void *data)
 void robot1TrackbarsCreator(std::string window_name, void *data)
 {
 
-    createTrackbar("H Def Min", window_name, &HDefMin, 179, setHDefMin, data);
-    createTrackbar("H Def Max", window_name, &HDefMax, 179, setHDefMax, data);
+    createTrackbar("H Def Min", window_name, &HDefMin, 180, setHDefMin, data);
+    createTrackbar("H Def Max", window_name, &HDefMax, 180, setHDefMax, data);
     createTrackbar("S Def Min", window_name, &SDefMin, 255, setSDefMin, data);
     createTrackbar("S Def Max", window_name, &SDefMax, 255, setSDefMax, data);
     createTrackbar("V Def Min", window_name, &VDefMin, 255, setVDefMin, data);
     createTrackbar("V Def Max", window_name, &VDefMax, 255, setVDefMax, data);
 
-    createTrackbar("H Dir Min", window_name, &H1DirMin, 179, setHDirectMin, data);
-    createTrackbar("H Dir Max", window_name, &H1DirMax, 179, setHDirectMax, data);
+    createTrackbar("H Dir Min", window_name, &H1DirMin, 180, setHDirectMin, data);
+    createTrackbar("H Dir Max", window_name, &H1DirMax, 180, setHDirectMax, data);
     createTrackbar("S Dir Min", window_name, &S1DirMin, 255, setSDirectMin, data);
     createTrackbar("S Dir Max", window_name, &S1DirMax, 255, setSDirectMax, data);
     createTrackbar("V Dir Min", window_name, &V1DirMin, 255, setVDirectMin, data);
@@ -364,8 +356,8 @@ void robot1TrackbarsCreator(std::string window_name, void *data)
 void robot2TrackbarsCreator(std::string window_name, void *data)
 {
 
-    createTrackbar("H Dir Min", window_name, &H2DirMin, 179, setHDirectMin, data);
-    createTrackbar("H Dir Max", window_name, &H2DirMax, 179, setHDirectMax, data);
+    createTrackbar("H Dir Min", window_name, &H2DirMin, 180, setHDirectMin, data);
+    createTrackbar("H Dir Max", window_name, &H2DirMax, 180, setHDirectMax, data);
     createTrackbar("S Dir Min", window_name, &S2DirMin, 255, setSDirectMin, data);
     createTrackbar("S Dir Max", window_name, &S2DirMax, 255, setSDirectMax, data);
     createTrackbar("V Dir Min", window_name, &V2DirMin, 255, setVDirectMin, data);
@@ -375,8 +367,8 @@ void robot2TrackbarsCreator(std::string window_name, void *data)
 void robot3TrackbarsCreator(std::string window_name, void *data)
 {
 
-    createTrackbar("H Dir Min", window_name, &H3DirMin, 179, setHDirectMin, data);
-    createTrackbar("H Dir Max", window_name, &H3DirMax, 179, setHDirectMax, data);
+    createTrackbar("H Dir Min", window_name, &H3DirMin, 180, setHDirectMin, data);
+    createTrackbar("H Dir Max", window_name, &H3DirMax, 180, setHDirectMax, data);
     createTrackbar("S Dir Min", window_name, &S3DirMin, 255, setSDirectMin, data);
     createTrackbar("S Dir Max", window_name, &S3DirMax, 255, setSDirectMax, data);
     createTrackbar("V Dir Min", window_name, &V3DirMin, 255, setVDirectMin, data);
@@ -385,8 +377,8 @@ void robot3TrackbarsCreator(std::string window_name, void *data)
 
 void ballTrackbarsCreator(std::string window_name, void *data)
 {
-    createTrackbar("H Ball Min", window_name, &ballHMin, 179, setBallHMin, data);
-    createTrackbar("H Ball Max", window_name, &ballHMax, 179, setBallHMax, data);
+    createTrackbar("H Ball Min", window_name, &ballHMin, 180, setBallHMin, data);
+    createTrackbar("H Ball Max", window_name, &ballHMax, 180, setBallHMax, data);
     createTrackbar("S Ball Min", window_name, &ballSMin, 255, setBallSMin, data);
     createTrackbar("S Ball Max", window_name, &ballSMax, 255, setBallSMax, data);
     createTrackbar("V Ball Min", window_name, &ballVMin, 255, setBallVMin, data);
